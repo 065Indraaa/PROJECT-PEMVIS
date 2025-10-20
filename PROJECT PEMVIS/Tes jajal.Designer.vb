70212015ed2a93fd06bafb352db1abe46a039cee
065Indraaa <indrbchtr06@gmail.com>
2025-10-20 22:10:41 +0700

    Tes Indra

--- PROJECT PEMVIS/Tes jajal.Designer.vb
+++ PROJECT PEMVIS/Tes jajal.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Tes_jajal
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(322, 106)
+        Me.Label1.Location = New System.Drawing.Point(333, 98)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(53, 13)
         Me.Label1.TabIndex = 0
