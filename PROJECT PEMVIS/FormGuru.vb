84d0a9ea2da5235b340f40936c63fe803ecd31d6
065Indraaa <indrbchtr06@gmail.com>
2025-10-22 22:52:51 +0700

    update

--- PROJECT PEMVIS/FormGuru.vb
+++ PROJECT PEMVIS/FormGuru.vb
@@ -1,32 +1,39 @@
 ﻿Public Class FormGuru
-    Private Sub LoadForm(FormGuru As Form)
-        Panel2.Controls.Clear() ' 
-        FormGuru.TopLevel = False
-        FormGuru.FormBorderStyle = FormBorderStyle.None
-        FormGuru = DockStyle.Fill
-        Panel2.Controls.Add(FormGuru)
-        FormGuru.Show()
 
+    ' Method untuk menampilkan form di dalam panel
+    Private Sub LoadForm(frm As Form)
+        Panel2.Controls.Clear()
+        frm.TopLevel = False
+        frm.FormBorderStyle = FormBorderStyle.None
+        frm.Dock = DockStyle.Fill
+        Panel2.Controls.Add(frm)
+        frm.Show()
     End Sub
 
-
-    Private Sub Button1_Click()
+    ' Tombol 1: buka FormCatatan
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         LoadForm(New FormCatatan())
     End Sub
 
-    Private Sub Button3_Click()
-
+    ' Tombol 2: contoh nanti bisa diarahkan ke form lain
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' Contoh: LoadForm(New FormDataSiswa())
     End Sub
 
-    Private Sub Button2_Click()
-
+    ' Tombol 3: contoh
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Contoh: LoadForm(New FormNilai())
     End Sub
 
-    Private Sub Button5_Click()
-
+    ' Tombol 5: contoh
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        ' Contoh: LoadForm(New FormProfilGuru())
     End Sub
 
-    Private Sub FormGuru_Load()
-
+    ' Saat form utama guru dibuka
+    Private Sub FormGuru_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Misalnya langsung tampilkan FormCatatan pertama kali
+        LoadForm(New FormCatatan())
     End Sub
+
 End Class
